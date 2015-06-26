be61de300d12da19a460e0019aab61f58223826d
tobigun <hennymcc@yahoo.de>
2015-06-27 01:09:38 +0200

    adjust repo branch

--- kminilte.sh
+++ kminilte.sh
@@ -18,20 +18,20 @@ cd -
 cd frameworks/av
 
 # Add Samsung WFD Service -- required for HWComposer
-git fetch https://github.com/cm-gardalte/android_frameworks_av.git cm-12.0
+git fetch https://github.com/cm-gardalte/android_frameworks_av.git cm-12.1
 git cherry-pick d4848efbd5650a48f93a784e5de248a13b830c04
 cd -
 
 cd frameworks/native
 
 # Revert "Remove Parcel::writeIntPtr."
-git fetch https://github.com/cm-gardalte/android_frameworks_native.git cm-12.0
+git fetch https://github.com/cm-gardalte/android_frameworks_native.git cm-12.1
 git cherry-pick ce3244f93e8c42ba0f67b449647d2c2e2b5c2f76
 cd -
 
 cd frameworks/base
 
 # Fix volume key music controls and wake up
-#git fetch https://github.com/cm-gardalte/android_frameworks_base.git cm-12.0
+#git fetch https://github.com/cm-gardalte/android_frameworks_base.git cm-12.1
 #git cherry-pick f92549d029b64a6a822d155cf76179f1e280bdcd
 cd -
